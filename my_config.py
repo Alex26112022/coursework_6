1df53cacd688d7d16400bce2785a154ef8c8de10
Алексей Денисенко <elvis.presly.039127@gmail.com>
2024-06-24 18:28:54 +0300

    base structure

--- my_config.py
+++ my_config.py
@@ -1,7 +1,11 @@
+import os
 from configparser import ConfigParser
 
+ROOT_DIR = os.path.dirname(__file__)
+database_params = os.path.join(ROOT_DIR, 'database.ini')
 
-def config(filename="database.ini", section="postgresql"):
+
+def config(filename=database_params, section="postgresql"):
     # create a parser
     parser = ConfigParser()
     # read config file
@@ -10,7 +14,7 @@ def config(filename="database.ini", section="postgresql"):
     if parser.has_section(section):
         params = parser.items(section)
         for param in params:
-            db[param[0]] = param[1]
+            db[param[0].upper()] = param[1]
     else:
         raise Exception(
             'Section {0} is not found in the {1} file.'.format(section,

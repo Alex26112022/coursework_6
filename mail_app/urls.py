c37d59f9835c070047cb620eddb537e835c2bd86
Алексей Денисенко <elvis.presly.039127@gmail.com>
2024-06-24 22:08:49 +0300

    main frame

--- mail_app/urls.py
+++ mail_app/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
-from mail_app.views import index
+from mail_app.views import index, examples, client
 from .apps import MailAppConfig
 
 app_name = MailAppConfig.name
 
 urlpatterns = [
     path('', index, name='mail_list'),
+    path('examples/', examples, name='examples'),
+    path('clients/', client, name='clients'),
 ]

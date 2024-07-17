ca30b092d0dc175b4b6b56d988ff37a87402215d
Алексей Денисенко <elvis.presly.039127@gmail.com>
2024-07-17 16:09:23 +0300

    login logout

--- users/urls.py
+++ users/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
 from users.apps import UsersConfig
@@ -11,4 +12,7 @@ urlpatterns = [
     path('email-confirm/<str:token>/', email_verification,
          name='email-confirm'),
     path('notification/', notification, name='notification'),
+    path('login/', LoginView.as_view(template_name='users/login.html'),
+         name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]

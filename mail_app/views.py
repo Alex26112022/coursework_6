faa83631138c38be7f1a1670ccb7b099d40adff7
Алексей Денисенко <elvis.presly.039127@gmail.com>
2024-07-18 13:09:03 +0300

    authenticated

--- mail_app/views.py
+++ mail_app/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, \
     DeleteView
@@ -7,11 +8,13 @@ from mail_app.models import Message, Client, Newsletter, MailingAttempt
 
 
 class NewsletterListView(ListView):
+    """ Выводит общую информацию о рассылках. """
     model = Newsletter
     paginate_by = 20
 
 
 class NewsletterDetailView(DetailView):
+    """ Выводит подробную информацию о рассылке. """
     model = Newsletter
 
     def get_context_data(self, **kwargs):
@@ -23,7 +26,8 @@ class NewsletterDetailView(DetailView):
         return context
 
 
-class NewsletterCreateView(CreateView):
+class NewsletterCreateView(LoginRequiredMixin, CreateView):
+    """ Создает новую рассылку. """
     model = Newsletter
     fields = ['title', 'first_sent_at', 'last_sent_at', 'status',
               'periodicity', 'message']
@@ -36,54 +40,64 @@ class NewsletterCreateView(CreateView):
         return super().form_valid(form)
 
 
-class NewsletterUpdateView(UpdateView):
+class NewsletterUpdateView(LoginRequiredMixin, UpdateView):
+    """ Редактирует рассылку. """
     model = Newsletter
     fields = ['title', 'first_sent_at', 'last_sent_at', 'status',
               'periodicity', 'message']
     success_url = reverse_lazy('mail_app:newsletter_list')
 
 
-class NewsletterDeleteView(DeleteView):
+class NewsletterDeleteView(LoginRequiredMixin, DeleteView):
+    """ Удаляет рассылку. """
     model = Newsletter
     success_url = reverse_lazy('mail_app:newsletter_list')
 
 
 class MessageListView(ListView):
+    """ Выводит общую информацию о сообщениях. """
     model = Message
     paginate_by = 6
 
 
 class MessageDetailView(DetailView):
+    """ Выводит подробную информацию о сообщении. """
     model = Message
 
 
-class MessageCreateView(CreateView):
-    model = Message
-    fields = '__all__'
-    success_url = reverse_lazy('mail_app:messages_list')
-
-
-class MessageUpdateView(UpdateView):
+class MessageCreateView(LoginRequiredMixin, CreateView):
+    """ Создает новое сообщение. """
     model = Message
     fields = '__all__'
     success_url = reverse_lazy('mail_app:messages_list')
 
 
-class MessageDeleteView(DeleteView):
+class MessageUpdateView(LoginRequiredMixin, UpdateView):
+    """ Редактирует сообщение. """
+    model = Message
+    fields = '__all__'
+    success_url = reverse_lazy('mail_app:messages_list')
+
+
+class MessageDeleteView(LoginRequiredMixin, DeleteView):
+    """ Удаляет сообщение. """
     model = Message
     success_url = reverse_lazy('mail_app:messages_list')
 
 
 class ClientListView(ListView):
+    """ Выводит общую информацию о клиентах. """
     model = Client
     paginate_by = 20
 
 
 class ClientDetailView(DetailView):
+    """ Выводит подробную информацию о клиенте. """
     model = Client
 
 
-class ClientCreateView(CreateView):
+class ClientCreateView(LoginRequiredMixin, CreateView):
+    """ Создает нового клиента. """
     model = Client
     fields = ['photo', 'email', 'name', 'surname', 'father_name', 'comment',
               'status', 'newsletter']
@@ -96,13 +110,15 @@ class ClientCreateView(CreateView):
         return super().form_valid(form)
 
 
-class ClientUpdateView(UpdateView):
+class ClientUpdateView(LoginRequiredMixin, UpdateView):
+    """ Редактирует клиента. """
     model = Client
     fields = ['photo', 'email', 'name', 'surname', 'father_name', 'comment',
               'status', 'newsletter']
     success_url = reverse_lazy('mail_app:clients_list')
 
 
-class ClientDeleteView(DeleteView):
+class ClientDeleteView(LoginRequiredMixin, DeleteView):
+    """ Удаляет клиента. """
     model = Client
     success_url = reverse_lazy('mail_app:clients_list')

cc6760a6eb4ea1e6f1e38973acf0bc9ed59e959c
Алексей Денисенко <elvis.presly.039127@gmail.com>
2024-08-03 18:26:38 +0300

    readme

--- blog/admin.py
+++ blog/admin.py
@@ -6,11 +6,10 @@ from blog.models import Blog
 
 @admin.register(Blog)
 class BlogAdmin(admin.ModelAdmin):
-    list_display = ['pk', 'title', 'content', 'preview', 'count_views',
-                    'created_at']
+    list_display = ['pk', 'title', 'preview', 'count_views', 'created_at']
     order_by = 'created_at'
     search_fields = ['title']
-    list_display_links = ['pk', 'title', 'content']
+    list_display_links = ['pk', 'title']
     readonly_fields = ['preview']
     fields = ['title', 'content', 'image', 'preview']
 
